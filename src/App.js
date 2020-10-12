80fe6db68b4c4f5313859afb0aeff9f9ecac335a
GrantW <Grant.Walker@ksdc.co.za>
2020-10-12 13:56:29 +0200

    Impl. Protected Routes (4h36m)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
-import { Home, Browse, Signup, Signin } from './pages';
+import { Home, Browse, SignUp, SignIn } from './pages';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 const App = () => {
+  const user = null;
+  // const user = { id: 1 };
+
   return (
     <Router>
       <Switch>
-        <Route exact path={ROUTES.HOME} component={Home} />
-        <Route path={ROUTES.BROWSE} component={Browse} />
-        <Route path={ROUTES.SIGNUP} component={Signup} />
-        <Route path={ROUTES.SIGNIN} component={Signin} />
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGNIN}>
+          <SignIn />
+        </IsUserRedirect>
+
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGNUP}>
+          <SignUp />
+        </IsUserRedirect>
         
-        <Redirect to={ROUTES.HOME} />
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+          <Home />
+        </IsUserRedirect>
+
       </Switch>
+
     </Router>
   );
 }
---
+++ src/helpers/routes.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+export function IsUserRedirect({ user, loggedInPath, children, ...restProps }) {
+  return (
+    <Route
+      {...restProps}
+      render={() => {
+        return !user
+          ? children
+          : <Redirect
+            to={{
+              pathname: loggedInPath,
+            }}
+          />
+      }}
+    />
+  )
+};
+
+export function ProtectedRoute({ user, children, ...restProps }) {
+  return (
+    <Route
+      {...restProps}
+      render={({ location }) => {
+        return user
+          ? children
+          : <Redirect
+            to={{
+              pathname: 'signin',
+              state: { from: location },
+            }}
+          />
+      }}
+    />
+  );
+}
