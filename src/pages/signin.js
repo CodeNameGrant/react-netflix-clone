241f2752442b28d98fe1f0ac05ee221905ee6b20
GrantW <Grant.Walker@ksdc.co.za>
2020-10-12 09:14:20 +0200

    Created SignIn Page (4h00m)

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,10 +1,74 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
 import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
+import { Form } from '../components';
+import * as ROUTES from '../constants/routes';
 
 export default function Signin() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext)
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const isInvalid = emailAddress.trim() === '' || password === '';
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+
+    // TODO Firebase work Here
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        // Push to Browse Page
+        history.push(ROUTES.BROWSE);
+      })
+      .catch(error => {
+        // console.log({error})
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+      })
+
+  }
+
   return (
-    <HeaderContainer>
-      Hello from SignIn Page
-    </HeaderContainer>
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignIn} method='POST'>
+            <Form.Input
+              placeholder='Email Address'
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)} />
+
+            <Form.Input
+              type='password'
+              autocomplete='off'
+              placeholder='Password'
+              value={password}
+              onChange={({ target }) => setPassword(target.value)} />
+
+            <Form.Submit type='submit' disabled={isInvalid}>Sign In</Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            New to Netflix <Form.Link to='/signup'>Sign Up Now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot.
+            Learn More.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+
+      <FooterContainer />
+    </>
   )
 }
