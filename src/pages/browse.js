a1b450566c2551fbb15f5f02ea7bbb57ad38117c
GrantW <Grant.Walker@ksdc.co.za>
2020-10-12 17:58:05 +0200

    Created BrowseContainer (5h07m)

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
+import { BrowseContainer } from '../containers/browse';
 
 export default function Browse() {
+  // we need the series and filmns
+  // we need slides
+  // pass it to the browse container
+
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+
+  const slides = selectionFilter({ series, films })
+  console.log(slides)
+  
   return (
-    <div>
-      Hello from Browse Page
-    </div>
+    <BrowseContainer slides={slides} />
   )
 }
